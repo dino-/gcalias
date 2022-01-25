07f53c6dea38bfbd6c03145891ea39e873347659
Dino Morelli <dino@ui3.info>
2022-01-25 09:57:15 -0500

    Added some of the usage text
    This needs more work.

--- src/app/GcAlias/Opts.hs
+++ src/app/GcAlias/Opts.hs
@@ -85,9 +85,10 @@ parseOpts = do
 
 
 footer' :: InfoMod a
-footer' = footerDoc . Just . string . printf content . showVersion $ version
-    where content = [here|OVERVIEW
+footer' = footerDoc . Just . string
+    $ printf content (op CsvPath myContactsPath) (showVersion version)
+    where content = [here|gcalias is a tool for constructing a static mutt aliases file from a dump of Google Contacts in CSV format. The usage pattern would be: 1) Occasionally dump your contacts with Google Takeout <https://takeout.google.com/> 2) Run this tool on the .tgz file, piping the output to your mutt aliases file path.
 
-gcalias is a tool for ...
+The optional CSV_PATH argument defaults to "%s" and refers to a path inside the tgz archive received from Google.
 
 Version %s  Dino Morelli <dino@ui3.info>|]
